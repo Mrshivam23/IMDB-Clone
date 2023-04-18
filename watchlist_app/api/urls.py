51ef202ffd4af376804f7d7e1da100fa029595ac
mrshivam23 <mrshivam23@outlook.com>
2023-04-18 11:14:59 +0530

    code commit

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,15 +1,25 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 # from watchlist_app.api.views import movie_list, movie_details
 from watchlist_app.api.views import *
+
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS,basename ='streamplatform')
 urlpatterns = [
     path('list/', WatchListListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchListDetailAV.as_view(), name='movie-detail'),
-    path('stream/', StreamPlatformAV.as_view(), name = 'Stream'),
-    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name = 'Stream-detail'),
-    path('review/',ReviewList.as_view(), name = 'Review-list'),
     
-    path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name = 'Stream-detail'),
-    path('stream/review/<int:pk>', ReviewDetail.as_view(), name = 'Stream-detail'),
+    path('',include(router.urls)),
+    
+    # path('stream/', StreamPlatformAV.as_view(), name = 'Stream'),
+    # path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name = 'Stream-detail'),
+    
+    # path('review/',ReviewList.as_view(), name = 'Review-list'),
+    # path('review/<int:pk>',ReviewDetail.as_view(), name = 'Review-detail'),
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name = 'Review-create'),
+    
+    path('stream/<int:pk>/review', ReviewList.as_view(), name = 'Review-list'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name = 'Review-detail'),
 
     # path('review/<int:pk>',ReviewDetail.as_view(), name = 'Review-detail'),
     
--- watchmate/urls.py
+++ watchmate/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('watch/', include('watchlist_app.api.urls'))
+    path('watch/', include('watchlist_app.api.urls')),
+    # path('stream/', include('watchlist_app.api.urls'))
 ]
