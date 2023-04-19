f35881eed0fa463f1ab30daec5af0e6b610fda10
mrshivam23 <mrshivam23@outlook.com>
2023-04-19 15:51:19 +0530

    token authentication

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user_app.api.views import registration_view
+from user_app.api.views import registration_view,logout_view
 
 
 urlpatterns = [
     path('login/', obtain_auth_token,name = 'login'),
     path('register/', view = registration_view,name = 'register'),
+    path('logout/', view = logout_view,name = 'logout'),
+    
     
 ]
