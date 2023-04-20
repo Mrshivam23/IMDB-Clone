7e8caf87b444e8343aed2bd9081fcb5f14af4aac
mrshivam23 <mrshivam23@outlook.com>
2023-04-20 11:12:19 +0530

    code commit

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -137,9 +137,11 @@ REST_FRAMEWORK = {
     # 'rest_framework_simplejwt.authentication.JWTAuthentication', 
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-    
-    'rest_framework.authentication.TokenAuthentication',
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # 'rest_framework.authentication.TokenAuthentication',
     ],
 }
 
-
+SIMPLE_JWT = {
+    'ROTATE_REFRESH_TOKEN': True,
+}
