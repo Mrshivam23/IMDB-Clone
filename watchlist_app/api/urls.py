afc048b0c25955ec83bbe4d1b725b7e45d8eb15c
mrshivam23 <mrshivam23@outlook.com>
2023-04-21 11:42:52 +0530

    test API

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -8,23 +8,16 @@ router.register('stream', StreamPlatformVS,basename ='streamplatform')
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchListDetailAV.as_view(), name='movie-detail'),
-    
     path('',include(router.urls)),
-    
+    path('<int:pk>/review-create/', ReviewCreate.as_view(), name = 'Review-create'),
+    path('<int:pk>/reviews/', ReviewList.as_view(), name = 'Review-list'),
+    path('reviews/<int:pk>/', ReviewDetail.as_view(), name = 'Review-detail'),
+    path('reviews/', UserReview.as_view(), name = 'user-review-detail'),
+    path('list2/', WatchListGV.as_view(), name = 'watch-detail'),
     # path('stream/', StreamPlatformAV.as_view(), name = 'Stream'),
     # path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name = 'Stream-detail'),
-    
     # path('review/',ReviewList.as_view(), name = 'Review-list'),
     # path('review/<int:pk>',ReviewDetail.as_view(), name = 'Review-detail'),
-    path('<int:pk>/review-create/', ReviewCreate.as_view(), name = 'Review-create'),
     
-    path('<int:pk>/reviews/', ReviewList.as_view(), name = 'Review-list'),
-    path('review/<int:pk>/', ReviewDetail.as_view(), name = 'Review-detail'),
-    path('review/', UserReview.as_view(), name = 'user-review-detail'),
-    path('list2/', WatchListGV.as_view(), name = 'watch-detail'),
-    
-    
-
-    # path('review/<int:pk>',ReviewDetail.as_view(), name = 'Review-detail'),
     
 ]
--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from watchlist_app.api import serializers
+from watchlist_app.models import models
 
 
+class StreamPlatformTestCase(APITestCase):
+    
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username = 'example',
+                                            password='password')
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token'+self.token.key)
+        
+        self.stream = models.StreamPlatform.objects.create(name="netflix",about="Best Platform",website="http://www.netflix.com")
+        
+    def test_streamplatform_create(self):
+        data = {
+            "name": "netflix",
+            "about": "Best Platform",
+            "website": "http://www.netflix.com"
+        }
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+    def test_streamplatform_list(self):
+        response = self.client.get(reverse('streamplatform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        
+    def test_streamplatform_ind(self):
+        response = self.client.get(reverse('streamplatform-detail',args=(self.stream.id,))) 
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        
+        
+    def test_review_user(self):
+        response = self.client.get('/watch/reviews/?username' )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)    
